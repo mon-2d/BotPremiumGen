04677666098f5423c5adc441b2ecf6280678105f
mon <ko0416595@gmail.com>
2025-08-27 18:55:45 +0700

    Update myserver.py

--- myserver.py
+++ myserver.py
@@ -1,15 +1,1 @@
-from flask import flask
-from threading import thread
 
-app = flask("")
-
-@app.route('/')
-    def home():
-        return "Server Runing!"
-
-def run():
-    app.run(host='0.0.0.0',port=8888)
-
-def server_on
-    t = thread(targer=run)
-    t.start()
